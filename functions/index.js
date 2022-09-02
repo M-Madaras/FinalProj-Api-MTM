38bb24bdacdc1eecbf63614e7990abfcbc1b0ee4
“Mason <masonmadaras@gmail.com>
2022-09-02 17:17:42 -0400

    the backend is posting to api

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,10 @@
 import functions from "firebase-functions";
 import express from 'express';
 import cors from 'cors';
-import { getGoals, setGoal, updateGoals, deleteGoal} from "./src/goals.js";
-import { updateGoals, setGoal } from "./src/goals.js";
+import { getGoals, setGoal, updateGoals, deleteGoal}
+ from "./src/goals.js";
+ import { createUser, loginUser } from "./src/users.js";
+
 
 const app = express();// making are app express
 app.use(cors()); // allowing our to use cors
@@ -13,6 +15,10 @@ app.get('/goals', getGoals);
 app.post('/goals', setGoal);
 app.patch('/goals/:goalId', updateGoals);
 app.delete('/goals/:goalId', deleteGoal);
+
+
+app.post('/users', createUser);
+app.post('/users/login', loginUser);
 
 // app.get('/test', (req, res) => res.send('TEST PASSED'));
 
--- functions/src/goals.js
+++ functions/src/goals.js
@@ -1,36 +1,51 @@
-import dbConnect from "./dbConnect.js";
+import jwt from 'jsonwebtoken';
+import dbConnect from './dbConnect.js';
+import { secretKey } from '../credentials.js';
 
-export async function getGoals(req, res){ // getting the db info
+export async function getGoals(req, res){ 
+    // const token = req.headers.authorization;
+    // const user = jwt.verify(token, secretKey);
     const db = dbConnect();
-    const collection = await db.collection('goals').get()
+    const collection = await db.collection('goals')
+    // .where('userId', '==', user.id)
+    .get()
     .catch(err => res.status(500).send(err));
     const goals = collection.docs.map(doc => {
         let goal = doc.data();
         goal.id = goal.id;
         return goal;
-    })
+    }
+    )
     res.send(goals);
 }
 
 export async function setGoal(req, res) {// set goal function adding new goal
-    const newGoal = req.body;
-    if (!newGoal || !newGoal.goal) { // bad request error
+    // const token = req.headers.authorization;
+    let newGoal = req.body;
+    // const user = jwt.verify(token, secretKey);
+    if (!newGoal || !newGoal.goal ) { // bad request error
         res.status(400).send({ success: false, message: 'Invalid request'});
         return;
     } 
+    // newTask.userId = user.id;
     const db = dbConnect();
     await db.collection('goals').add(newGoal)
     .catch(err => res.status(500).send(err)); // eternal server error
-    res.status(201).send('ok');// created status code
+    res.status(201);// created status code
     getGoals(req, res) // getting back full list of goal
+    // res.send(newGoal)
 }
 
-export function updateGoals(req, res) {
-    const goalsUpdate = req.body;
+export async function updateGoals(req, res) {
+    const goalUpdate = req.body;
     const { goalId } = req.params;
     res.status(202).send('Goals Updated');
- }
-
+    await db.collection('tasks').doc(goalId).update(goalUpdate)
+    .catch(err => res.status(500).send(err));
+  res.status(202);
+  getTasks(req, res);
+}
+ 
  export function deleteGoal(req, res) {
     const { goalId } = req.params;
     res.status(203).send('Goal Deleted');
