1bd3dd7b3df667f9ab81ff1fb673aa51ed773dce
“Mason <masonmadaras@gmail.com>
2022-09-08 13:27:52 -0400

    update works in backend

--- functions/src/goals.js
+++ functions/src/goals.js
@@ -2,14 +2,14 @@ import jwt from 'jsonwebtoken';
 import dbConnect from './dbConnect.js';
 import { secretKey } from '../credentials.js';
 
-export async function getGoals(req, res){ 
+export async function getGoals(req, res) {
     // const token = req.headers.authorization;
     // const user = jwt.verify(token, secretKey);
     const db = dbConnect();
     const collection = await db.collection('goals')
-    // .where('userId', '==', user.id)
-    .get()
-    .catch(err => res.status(500).send(err));
+        // .where('userId', '==', user.id)
+        .get()
+        .catch(err => res.status(500).send(err));
     const goals = collection.docs.map(doc => {
         let goal = doc.data();
         goal.id = goal.id;
@@ -23,14 +23,14 @@ export async function setGoal(req, res) {// set goal function adding new goal
     // const token = req.headers.authorization;
     let newGoal = req.body;
     // const user = jwt.verify(token, secretKey);
-    if (!newGoal || !newGoal.goal ) { // bad request error
-        res.status(400).send({ success: false, message: 'Invalid request'});
+    if (!newGoal || !newGoal.goal) { // bad request error
+        res.status(400).send({ success: false, message: 'Invalid request' });
         return;
-    } 
+    }
     // newTask.userId = user.id;
     const db = dbConnect();
     await db.collection('goals').add(newGoal)
-    .catch(err => res.status(500).send(err)); // eternal server error
+        .catch(err => res.status(500).send(err)); // eternal server error
     res.status(201);// created status code
     getGoals(req, res) // getting back full list of goal
     // res.send(newGoal)
@@ -39,14 +39,17 @@ export async function setGoal(req, res) {// set goal function adding new goal
 export async function updateGoals(req, res) {
     let goalUpdate = req.body;
     const { goalId } = req.params;
-    res.status(202).send('Goals Updated');
-    await db.collection('tasks').doc(goalId).update(goalUpdate)
-    .catch(err => res.status(500).send(err));
-  res.status(202);
-  getTasks(req, res);
+    const db = dbConnect();
+    await db.collection('goals').doc(goalId).update(goalUpdate)
+        .catch(err => res.status(500).send(err));
+    res.status(202)
+
+    getGoals(req, res);
 }
- 
- export function deleteGoal(req, res) {
+
+export function deleteGoal(req, res) {
+    let goalDelete = req.body;
     const { goalId } = req.params;
     res.status(203).send('Goal Deleted');
- }
+
+}
